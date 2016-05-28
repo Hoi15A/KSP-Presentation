be494f51ecdbda29c8d8696dd5ec668e33dd1766
fuerbringer <severin@protonmail.ch>
2016-05-28 02:48:00 +0200

    Added basic instructions on slide 0

--- code/presentation.js
+++ code/presentation.js
@@ -10,20 +10,10 @@
 	var sidebar = document.getElementById("sidebar");
 	var sidebar_button = document.getElementById("sidebar_button");
 	var slide_count = document.getElementsByClassName("slide").length;
-	sidebar.style.display = "none"; // Sidebar hidden on load
 
-	for(var i = 0; i < slide_count; i++) {
-		var newDiv = document.createElement("div");
-		var heightPercent = 100 / slide_count;
-		newDiv.className = "sidebar_element";
-		newDiv.style = "height: " + heightPercent + "%";
-		newDiv.textContent = i + 1;
-		newDiv.onclick = function () {
-			previous = current;
-			current = this.textContent - 1;		
-			scroll();
-		}
-		sidebar.appendChild(newDiv);
+	window.onload = function() {
+		sidebar.style.display = "none"; // Sidebar hidden on load
+		generateSidebar();
 	}
 
 	window.onbeforeunload = function() {
@@ -61,26 +51,83 @@
 		sidebar_button.style.right = sidebar.style.display == "none" ? "0" : "1.5vw";
 	}
 
+	function generateSidebar() {
+		for(var i = 0; i < slide_count; i++) {
+			var newDiv = document.createElement("div");
+			var heightPercent = 100 / slide_count;
+			newDiv.className = "sidebar_element";
+			newDiv.style = "height: " + heightPercent + "vh";
+			newDiv.textContent = i + 1;
+			newDiv.onclick = function () {
+				var nextUp = parseInt(this.textContent) - 1;
+				console.log(current);
+				console.log(nextUp);
+				if(nextUp > current) {
+					current = nextUp - 1;
+					scrollNext();
+				} else if(nextUp < current) {
+					current = nextUp + 1;
+					scrollPrevious();
+				}
+				
+			}
+			sidebar.appendChild(newDiv);
+		}
+	}
+
 	function scrollNext() {
 		current = current < slides_count - 1 ? current + 1 : current;
+		// Special case for first slide
+		if(current == 1) {
+			// Second slide, instructions not needed now
+			instructionsOut();
+		}
 		scroll();
 	}
 
 	function scrollPrevious() {
-		current = current > 0 ? current - 1 : current;;
+		current = current > 0 ? current - 1 : current;
+		// Special case for first slide
+		if(current == 0) {
+			// Bring back instructions
+			instructionsIn();
+		}
 		scroll();
 	}
 
-	function scroll() {
-		// Slide changed
-		// Animate next slide
-		slides[current].className = "slide animated zoomIn";
-		slides[previous].className = "slide slide_exit animated fadeOut";
+	function instructionsIn() {
+		var elems = document.getElementsByClassName("instructions");
+		for(var i = 0; i < elems.length; i++) {
+			if(i % 2 == 0)
+				elems[i].className = "instructions animated bounceInLeft";
+			else
+				elems[i].className = "instructions animated bounceInRight";
+		}
+	}
 
-		// Scroll to next slide
-		$('html, body').animate({
-    		scrollTop: slides[current].offsetTop
-    	}, scroll_time_ms);
+	function instructionsOut() {
+		var elems = document.getElementsByClassName("instructions");
+		for(var i = 0; i < elems.length; i++) {
+			if(i % 2 == 0)
+				elems[i].className = "instructions animated bounceOutLeft";
+			else
+				elems[i].className = "instructions animated bounceOutRight";
+		}
+	}
+
+	function scroll() {
+		if(previous != current) {
+			// Slide changed
+			// Animate next slide
+			slides[current].className = "slide animated zoomIn";
+			slides[previous].className = "slide slide_exit animated fadeOut";
+
+			// Scroll to next slide
+			$('html, body').animate({
+    			scrollTop: slides[current].offsetTop
+    		}, scroll_time_ms);
+			previous = current;
+		}
 	}
 
 }) ();
